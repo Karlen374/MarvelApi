c7aa2e7202d20df666a255ec34f2f5f15e64c51a
Karlen <svaryan01@inbox.ru>
2021-12-20 06:32:06 +0300

    react router

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,47 +1,22 @@
 import AppHeader from "../appHeader/AppHeader";
-import RandomChar from "../randomChar/RandomChar";
-import CharList from "../charList/CharList";
-import CharInfo from "../charInfo/CharInfo";
-import { useState} from "react";
-import decoration from '../../resources/img/vision.png';
-import ErrorBoundary from "../errorBoundary/ErrorBoundary";
-import ForTest from "../forTest/forTest";
-import ComicsList from "../comicsList/ComicsList";
-import SingleComic from "../singleComic/SingleComic";
+import { BrowserRouter as Router,Route,Routes} from "react-router-dom";
+import { MainPage,ComicsPage } from "../pages";
+
 
 const App =()=>{
-
-    const [selectedChar,setChar]=useState(null);
-
-    
-    const onCharSelected=(id)=>{
-        setChar(id);
-    }
-    
         return (
-            <div className="app">
-              {/* <ForTest/> */}
+           <Router>
+              <div className="app">
                 <AppHeader/>
-            <main>
-            
-              <SingleComic/>
-              {/* <ErrorBoundary>
-                <RandomChar onCharSelected={onCharSelected} />
-              </ErrorBoundary>
-                   
-              <div className="char__content">
-                <ErrorBoundary>
-                  <CharList onCharSelected={onCharSelected} />
-                </ErrorBoundary>
-                
-                <ErrorBoundary>
-                  <CharInfo charId={selectedChar}/>
-                </ErrorBoundary> 
-                       
-                    </div> */}
-                    <img className="bg-decoration" src={decoration} alt="vision"/>
+                <main>
+                  <Routes>
+                        <Route  path="/" element={<MainPage/>}/>
+                        <Route  path="/comics" element={<ComicsPage/>}/>
+                  </Routes>
+
                 </main>
             </div>
+           </Router>
         )
     }
    
