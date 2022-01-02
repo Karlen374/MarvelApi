a4e3cbef4baa9e5e7835b0bdad94e19e8a86c402
Karlen <svaryan01@inbox.ru>
2022-01-03 00:08:47 +0300

    React.lazy

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,7 +1,12 @@
+import {lazy,Suspense} from 'react';
 import AppHeader from "../appHeader/AppHeader";
 import { BrowserRouter as Router,Route,Routes} from "react-router-dom";
-import { MainPage,ComicsPage,Page404 ,SingleComicPage} from "../pages";
 
+import Spinner from '../spinner/Spinner';
+const Page404=lazy(()=>import('../pages/404'));//Динамический импорт
+const MainPage=lazy(()=>import('../pages/MainPage'))
+const ComicsPage=lazy(()=>import('../pages/ComicsPage'))
+const SingleComicPage=lazy(()=>import('../pages/SingleComicPage'))
 
 const App =()=>{
         return (
@@ -9,12 +14,15 @@ const App =()=>{
               <div className="app">
                 <AppHeader/>
                 <main>
-                  <Routes>
+                  <Suspense fallback={<Spinner/>}>{/*Для динамического импорта */}
+                    <Routes>
                         <Route  path="/" element={<MainPage/>}/>
                         <Route  path="/comics" element={<ComicsPage/>}/>
                         <Route  path="/comics/:comicId" element={<SingleComicPage/>}/>
                         <Route path="*" element={<Page404/>}/>
-                  </Routes>
+                    </Routes>
+                  </Suspense>
+              
                   
                 </main>
             </div>
